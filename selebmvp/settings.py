e004ec1a51f208b82f791853a189886f0e9d09c8
sandman <santosh.mano@gmail.com>
2016-08-21 11:54:29 -0700

    EB Stuff

--- selebmvp/settings.py
+++ selebmvp/settings.py
@@ -186,7 +186,7 @@ USE_TZ = env('USE_TZ', default=True)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
