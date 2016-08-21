5189fd31f96ae872f7c746239e6cc35ba10f9d7c
sandman <santosh.mano@gmail.com>
2016-08-21 12:22:05 -0700

    EB Stuff

--- selebmvp/settings.py
+++ selebmvp/settings.py
@@ -85,18 +85,7 @@ WSGI_APPLICATION = 'selebmvp.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DB_BACKEND = env('DB_BACKEND')
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-elif DB_BACKEND == 'SQLITE3':
+if DB_BACKEND == 'SQLITE3':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
